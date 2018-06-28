c7942346a5c84bea7de5d0fc68dd4d6e1393f100
demet kızılbuga <demetkizilbuga@gmail.com>
2018-06-28 14:37:55 +0300

    image_open

---
+++ image_open/opencv.py
@@ -0,0 +1,5 @@
+import cv2
+
+img = cv2.imread('/home/demet/Belgeler/foto-kirpma/demet.png')
+cv2.imshow('image',img)
+cv2.waitKey(0)
---
+++ image_open/pillow.py
@@ -0,0 +1,5 @@
+from PIL import Image
+#img=Image.open("/home/demet/Belgeler/foto-kirpma/demet.png")
+img=Image.open("demet.png")
+
+img.show()
